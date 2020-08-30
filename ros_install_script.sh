a707b363e448aa57fbe667abb2320949973a6be4
jmfajardod <jmfajardod@unal.edu.co>
2020-08-29 21:13:12 -0500

    Added Kinetic files

---
+++ ros_install_script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Request Admin privileges
+sudo -s
+
+# Setup your sources.list
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# Set up your keys
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+# Update packages
+apt-get update
+
+# Install ROS
+apt-get install ros-kinetic-desktop-full
+
+# Environment setup
+echo "# Source ROS installation setup"  >> ~/.bashrc
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# Dependencies for building packages
+apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+# Install rosdep
+apt install python-rosdep
+
+# Initialize rosdep
+rosdep init
+rosdep update
+
+# Print final message
+echo "Script execution finalized"
